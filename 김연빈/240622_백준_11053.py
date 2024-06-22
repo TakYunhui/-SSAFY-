6bf5ecbeb33f702ad4cb9c74f1109f5d0c983bed
김연빈 <kyb1434@gmail.com>
2024-06-22 14:07:06 +0900

    sol: 240622_백준_11053

---
+++ 김연빈/240622_백준_11053.py
@@ -0,0 +1,18 @@
+# 가장 긴 증가하는 부분 수열
+
+N = int(input())
+arr = list(map(int, input().split()))
+
+start = arr[0]
+target = arr[0]
+ans = [1] * N
+
+for i in range (1, N):
+    # 작은 것들 중에 가장 ans가 큰거+1
+    maxi = 0
+    for j in range(i):
+        if arr[j] < arr[i] and maxi < ans[j]:
+            maxi = ans[j]
+    ans[i] = maxi + 1
+
+print(max(ans))

0a5cfa329a19bea9e853ed2e7ac7a381ec12605c
김연빈 <kyb1434@gmail.com>
2024-08-03 18:01:38 +0900

    unsolve: 240802_백준_2108_시간초과

---
+++ 김연빈/240802_백준_2108.py
@@ -0,0 +1,30 @@
+# 통계학
+
+N = int(input())
+
+arr = []
+total = 0
+nums = dict()
+often = (1, 0)
+
+for _ in range(N):
+    num = int(input())
+    arr.append(num)
+    total += num
+    tmp = nums.get(num)
+    if (_ == 0):
+        often = (1, num)
+    if (tmp==None):
+        nums[num] = 1
+    else:
+        nums[num] = tmp+1
+        if (tmp+1 > often[0]):
+            often = (tmp+1, num)
+        
+
+arr.sort()
+
+print(round(total/N))
+print(arr[N//2]) # 중앙
+print(often[1]) # 최빈
+print(arr[-1]-arr[0]) # 범위

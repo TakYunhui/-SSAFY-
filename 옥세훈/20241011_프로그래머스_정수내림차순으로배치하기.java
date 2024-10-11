c110d65317c4d353c5ef4edf333805375cd3a0ea
옥세훈 <sehun9803@gmail.com>
2024-10-11 22:59:08 +0900

    sol: programmers_2 java problems

--- 옥세훈/20241011_프로그래머스_정수내림차순으로배치하기.java
+++ 옥세훈/20241011_프로그래머스_정수내림차순으로배치하기.java
@@ -1,21 +1,28 @@
+import java.util.Arrays;
+import java.util.Collections;
 
 class Solution {
     public long solution(long n) {
-        long answer = 0;
+        // 정수를 문자열로 변환
+        String new_n = String.valueOf(n);
+        // 각 자리수를 저장할 배열 생성
         Character[] arr = new Character[new_n.length()];
-        
-        for (int i=0; i < new_n.length(); i++){
+
+        // 각 자리수를 배열에 추가
+        for (int i = 0; i < new_n.length(); i++) {
             arr[i] = new_n.charAt(i);
         }
-        
-        // char 형태로도 숫자들은 크기 비교를 통해 정렬할 수 있다.
-        // 자바에서 int 형 배열은 역정렬하는 함수가 없어서 직접 만들어야 한다.
+
+        // 배열을 내림차순으로 정렬
         Arrays.sort(arr, Collections.reverseOrder());
-        
+
+        // 정렬된 문자열을 정수로 변환
         StringBuilder answer = new StringBuilder();
         for (char c : arr) {
             answer.append(c);
         }
-        return answer;
+
+        return Long.parseLong(answer.toString()); // 정수로 반환
     }
+
 }

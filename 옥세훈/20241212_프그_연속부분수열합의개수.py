fe87f73e7bf62f820a5b265d1605fcdabc4c8188
옥세훈 <sehun9803@gmail.com>
2024-12-12 22:49:16 +0900

    sol: 프그_연속부분수열합의개수

---
+++ 옥세훈/20241212_프그_연속부분수열합의개수.py
@@ -0,0 +1,12 @@
+def solution(elements):
+    answer = set()
+    
+    element_len = len(elements)
+    elements = elements * 2
+    # print(elements)
+    
+    for i in range(element_len):
+        for j in range(element_len):
+            answer.add(sum(elements[j:j+i+1]))
+    
+    return len(answer)

7e5d2fb17c558e82fd64407cb7c28ca6d0e88784
탁윤희 <gwalgwal98@naver.com>
2024-12-15 16:06:49 +0900

    Solve: 백준 24445. 알고리즘 수업 - 너비 우선 탐색 2

---
+++ 탁윤희/241215_백준_24445.py
@@ -0,0 +1,34 @@
+# 실버2. 알고리즘 수업 - bfs 2
+# 정점, 간선, 시작점
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+n, m, r = map(int, input().split())
+# 인접 리스트 - 간선 양방향 연결
+graph = [[] for _ in range(n+1)]
+for _ in range(m):
+    u, v = map(int,input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+# 내림차순 방문을 위한 정렬
+for i in range(n+1):
+    graph[i].sort(reverse=True)
+# 시작점(r)에서부터 정점 i까지의 방문순서(거리) 출력
+q = deque([r])
+visited = [0] * (n+1)
+visited[r] = 1
+cnt = 2
+# bfs로 각 정점까지의 거리 visited에 업데이트 --> 순서가 아니네?
+
+while q:
+    x = q.popleft()
+
+    for node in graph[x]:
+        if not visited[node]:
+            visited[node] = cnt
+            cnt += 1
+            q.append(node)
+
+for i in range(1, n+1):
+    print(visited[i])

2d3b1dd6c38338c61e527154cc1695b83a6cb080
RosaDamascena <hsj990604@gmail.com>
2024-12-16 20:25:09 +0900

    Sol: 백준 2606 바이러스

--- 한성주/241216_백준_바이러스.cpp
+++ 한성주/241216_백준_바이러스.cpp
@@ -4,4 +4,30 @@
 using namespace std;
 
 int n, m, a, b;
+int cnt = 0;
 vector<int> arr[101];
+
+void DFS(int start, vector<bool>& visited) {
+    visited[start] = true;
+    for (int next : arr[start]) {
+        if (!visited[next]) {
+            cnt++;
+            DFS(next, visited);
+        }
+    }
+}
+
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i < m; i++) {
+        cin >> a >> b;
+        arr[a].push_back(b);
+        arr[b].push_back(a);
+    }
+
+    vector<bool> visited(n + 1, false);
+    DFS(1, visited);
+
+    cout << cnt;
+    return 0;
+}

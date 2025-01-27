f489418112a8cf708572990c82abc6cfb2f570e4
옥세훈 <sehun9803@gmail.com>
2025-01-27 21:05:36 +0900

    sol: 백준_에라토스테네스의체

--- 옥세훈/20250126_백준_에라토스테네스의체.py
+++ 옥세훈/20250126_백준_에라토스테네스의체.py
@@ -1,23 +1,13 @@
 n, k = map(int, input().split())
-arr = [i for i in range(2, n+1)]
-visit = [False for i in range(n+2)]
+visited = [False for _ in range(n+1)]
 cnt = 0
-res = 0
-p = 0
-while cnt < k:
+for i in range(2, n+1):
+    if not visited[i]:
+        for j in range(i, n+1, i):
+            if not visited[j]:
+                cnt += 1
+                visited[j] = True
 
-    for i in arr:
-        if not visit[i]:
-            p = i
-            cnt += 1
-            res = p
-            visit[i] = True
-            break
-
-    for j in range(n-1):
-        if not visit[arr[j]] and arr[j] % p == 0:
-            cnt += 1
-            res = arr[j]
-            visit[arr[j]] = True
-
-print(res)
+            if cnt == k:
+                print(j)
+                exit()

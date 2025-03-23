eb78f2ae2801cd32b803fae3256edf899bca6291
김연빈 <kyb1434@gmail.com>
2025-03-24 00:03:12 +0900

    Sol: 백준 9084 동전

--- 김연빈/250323_백준_9084.py
+++ 김연빈/250323_백준_9084.py
@@ -2,25 +2,20 @@
 T = int(input())
 for _ in range(T):
     N = int(input())
-    coins = list(map(int, input().split()))
+    coins = [0]+list(map(int, input().split()))
     M = int(input())
-    dp_list = [[0]*(M+2) for _ in range(coins[-1]+1)] #2d
+    dp_list = [[0]*(M+2) for _ in range(N+1)] #2d
 
-    # print(coins)
-    # print(dp_list)
-    for coin in coins:
+    for i in range(1, N+1):
         for total in range(1, M+1):
-            k = total - coin
-            # print(coin, total, k)
+            k = total - coins[i]
             if (k>0):
-                dp_list[coin][total] = dp_list[coin][k] + dp_list[coin-1][total]
+                dp_list[i][total] = dp_list[i][k] + dp_list[i-1][total]
             elif (k==0):
-                dp_list[coin][total] = dp_list[coin-1][total] + 1
+                dp_list[i][total] = dp_list[i-1][total] + 1
             else:
-                # print('c')
-                dp_list[coin][total] = dp_list[coin-1][total]
-                # print('a')
-    print(dp_list[coins[-1]][M])
+                dp_list[i][total] = dp_list[i-1][total]
+    print(dp_list[N][M])
 '''
 2 3
 0 0 1 2 3 4 5 6 ... 10
